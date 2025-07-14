8fbaefbc8581088028d17bebb6c3eaada53b5716
kruthikjs <kruthikjs15@gmail.com>
2025-07-14 22:32:02 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,5 @@ public class HelloWorld{
   public static void main(String[] args){
     System.out.println{"Helloworld");
     System.out.println{"Commit_change1");
-    System.out.println{"Commit_change2");
   }
 }
